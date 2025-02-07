3afcfadb567860fec81d48d98ea46263c7d365c1
Sugunstar <venkatasugunadithya@gmail.com>
2025-02-07 12:12:41 +0530

    modified qrc.py

--- qrc.py
+++ qrc.py
@@ -1,11 +1,15 @@
 import qrcode
+
+f=open('file.txt','r')
+st=f.read()
+
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data('')
+qr.add_data(st)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
