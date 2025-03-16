17fb9925a74e6745d4988f179952cdec92f623af
Sugunstar <venkatasugunadithya@gmail.com>
2025-03-16 17:13:24 +0530

    making changes to add data to database insted of file

--- data_input.py
+++ data_input.py
@@ -1,16 +1,15 @@
 import pymongo
 if __name__=="__main__":
-    client=pymongo.MongoClient("mongodb://localhost:27017")
-    name=input("enter your database name")
+    client=pymongo.MongoClient("mongodb://localhost:27017/")
+    name=input("enter your database name ")
     db=client[name]
     collection=db['list_items']
-    f=open("file.txt",'+a')
+    items=dict()
+    print(client)
 
     num=int(input("enter the number of items to be added"))
     for i in range(1,num+1):
         key=input(f"enter item {i}")
         value=input(f"enter value of {i}th item")
-        f.write(f"{key} --- {value}")
-        f.write("\n")
-    
-    
+        items.update({f'item {i}': key,f'data related to item {i}': value})
+    collection.insert_one(items)
